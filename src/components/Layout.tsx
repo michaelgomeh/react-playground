3ebdfcf12f428e9625bef913e6d864d3d2b84887
Michael Gomeh <gomehmichael@gmail.com>
2025-02-12 17:35:22 +0200

    navbar improvements

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,36 @@
-import { AppShell, AppShellMain, Group } from '@mantine/core';
+import {
+	AppShell,
+	AppShellMain,
+	Burger,
+	Group,
+	NavLink,
+	ScrollArea,
+} from '@mantine/core';
 import React, { ReactNode } from 'react';
 import { IconBrandReact } from '@tabler/icons-react';
+import { useDisclosure } from '@mantine/hooks';
 
 const Layout = ({ children }: { children: ReactNode }) => {
+	const [opened, { toggle }] = useDisclosure();
+
 	return (
-		<AppShell>
+		<AppShell
+			header={{ height: 60 }}
+			navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+			padding="md"
+		>
 			<AppShell.Header p={12}>
 				<Group>
+					<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
 					<IconBrandReact strokeWidth={1} />
 					React Playground
 				</Group>
 			</AppShell.Header>
+			<AppShell.Navbar>
+				<ScrollArea>
+					<NavLink href="/performance" label="Performance And Optimization" />
+				</ScrollArea>
+			</AppShell.Navbar>
 			<AppShellMain>{children}</AppShellMain>
 		</AppShell>
 	);
