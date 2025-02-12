ae27803ce5a5a151802f32a632e8526310e71827
Michael Gomeh <gomehmichael@gmail.com>
2025-02-12 17:08:39 +0200

    add performance page

---
+++ src/Pages/Performance.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PerformanceAndOptimizationPage = () => {
+	return <div>PerformancePage</div>;
+};
+
+export default PerformanceAndOptimizationPage;
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,17 @@ import './style/index.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from './Home';
+import PerformanceAndOptimizationPage from './Pages/Performance';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route
+					path="/performance"
+					element={<PerformanceAndOptimizationPage />}
+				/>
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>
